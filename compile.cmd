9c09d8bdabd569e48c315b58303944dcb671adcf
Ryan J Gray <ryan@ryanjgray.com>
2022-12-17 21:00:40 +0000

    build: Attempt to add support for shader precompilation x2.

--- compile.cmd
+++ compile.cmd
@@ -35,13 +35,6 @@ for /f "usebackq tokens=*" %%p in (`call "%VSWHERE%" -products * -latest -requir
 	set "MSBUILD=%%p"
 )
 
-@if not defined _echo echo off
-for /f "usebackq delims=" %%i in (`call "%VSWHERE%" -latest -property installationPath`) do (
-  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat" %*
-  )
-)
-
 rem Do we have a non-empty MSBuild path?
 if defined %MSBUILD% (
 	echo --------------[Build Configuration]--------------
@@ -118,6 +111,12 @@ if defined %MSBUILD% (
 	)
 
 	rem Precompile shaders to avoid runtime errors on Proton, due to unfinished Wine shader compiler code.
+    @if not defined _echo echo off
+    for /f "usebackq delims=" %%i in (`call "%VSWHERE%" -latest -property installationPath`) do (
+      if exist "%%i\Common7\Tools\vsdevcmd.bat" (
+        %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat"
+      )
+    )
 	fxc.exe /nologo /T ps_4_0 /E mainPS /O3 /Ges /Qstrip_reflect /Qstrip_debug /Fo data/shaders/compiled/sharpen_ps.hlsl data/shaders/sharpen_ps.hlsl
 	fxc.exe /nologo /T ps_4_0 /E mainPS /O3 /Ges /Qstrip_reflect /Qstrip_debug /Fo data/shaders/compiled/shadowLinearize_ps.hlsl data/shaders/shadowLinearize_ps.hlsl
 	fxc.exe /nologo /T ps_4_0 /E mainPS /O3 /Ges /Qstrip_reflect /Qstrip_debug /Fo data/shaders/compiled/shadowDownsample_ps.hlsl data/shaders/shadowDownsample_ps.hlsl
